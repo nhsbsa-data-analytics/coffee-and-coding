028a20e9ff90ed8b673e3394a1259df6e536b8ec
Adam Ivison <adam.ivison@nhs.net>
2021-04-26 13:10:17 +0000

    Add everything into `environment.yml`

--- install.R
+++
@@ -1,8 +0,0 @@
-# Load utils
-library(utils)
-
-# Install new cran packages
-cran_required_packages <- c(
-)
-new_cran_packages <- cran_required_packages[!(cran_required_packages %in% utils::installed.packages()[, "Package"])]
-if(length(new_cran_packages)) utils::install.packages(new_cran_packages)
