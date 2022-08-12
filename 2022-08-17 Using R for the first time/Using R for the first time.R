e9f66cad2d6e8722101da0542d57876c6a768596
nidod <nikki.dodds@nhs.net>
2022-08-12 15:24:02 +0100

    Update Using R for the first time.R

--- 2022-08-17 Using R for the first time/Using R for the first time.R
+++ 2022-08-17 Using R for the first time/Using R for the first time.R
@@ -1,1 +1,51 @@
-##use this file for code
+## 3.2 Install a package    
+## install.packages("tidyverse") type this code below
+
+
+## 3.3 Using a Package 
+## library(tidyverse)
+
+
+## 3.4 Getting help with a Package
+## help(package="dplyr")
+
+
+## 4.1 Get Working directory
+## getwd() 
+
+
+## 4.2 Set Working directory
+## setwd()
+
+## 5.1 Load CSV, from current directory
+## first_table <- read_csv("Mydata.csv")
+
+## 5.1 Load CSV, from a different directory
+## second_table <- read_csv("C:/Users/nidod/CSVS/Mydata2.csv")
+
+
+## 5.2 Load excel from current directory 
+## third_table <- read_excel("Mydata3.xlsx")
+
+
+## 5.2 Load excel from different directory
+##fourth_table <- read_excel("C:/Users/nidod/EXCEL/Mydata4.xlsx")
+
+
+## 6 Simple analysis in R - Get mtcars data
+## mtcars <- mtcars
+
+
+##6.1 count rows
+## nrow(mtcars)
+
+##6.2 count columns
+## ncol(mtcars)
+
+filter(mtcars, cyl == 4 & mpg > 30)
+Cylinders4_mpg <- mtcars %>% 
+  filter(cyl == "4" & mpg >"30")
+
+Merc_cars <- mtcars %>% 
+  filter( %like% "%merc%")
+
