77acf5a531b504b226e97d9a8c0831fefc7bbf3f
nidod <nikki.dodds@nhs.net>
2022-08-15 13:07:54 +0100

    Update Using R for the first time.R

--- 2022-08-17 Using R for the first time/Using R for the first time.R
+++ 2022-08-17 Using R for the first time/Using R for the first time.R
@@ -1,58 +1,105 @@
-# 3.2 Install a package    
+# 4.2 Install a package    
 # install.packages("tidyverse") type this code below
 
 
 
-# 3.3 Using a Package 
+# 4.3 Using a Package 
 # library(tidyverse)
 
 
-# 3.4 Getting help with a Package
+# 4.4 Getting help with a Package
 # help(package="dplyr")
 
 
-# 4.1 Get Working directory
+# 5.1 Get Working directory
 # getwd() 
 
 
-# 4.2 Set Working directory
+# 5.2 Set Working directory
 # setwd()
 
-# 5.1 Load CSV, from current directory
+# 6.1 Load CSV, from current directory
 # first_table <- read_csv("Mydata.csv")
 
-# 5.1 Load CSV, from a different directory
+
+
+# 6.1 Load CSV, from a different directory
 # second_table <- read_csv("C:/Users/cypher/CSVS/Mydata2.csv")
 
 
-# 5.2 Load excel from current directory 
+# 6.2 Load excel from current directory 
 # third_table <- read_excel("Mydata3.xlsx")
 
 
-# 5.2 Load excel from different directory
+# 6.2 Load excel from different directory
 # fourth_table <- read_excel("C:/Users/cypher/EXCEL/Mydata4.xlsx")
 
 
-# 6 Simple analysis in R - Get mtcars data
+# 7 Simple analysis in R - Get mtcars data
 # mtcars <- mtcars
 
 
-# 6.1 Sum up a column
+# 7.1 Sum up a column
 # num_cyl <- mtcars %>% 
 # summarise(sum(cyl))
 
-# 6.2 Filter rows
+
+
+# 7.2 Filter rows
 # Cylinders4 <- mtcars %>% 
 # filter(cyl == 4)
 
-# 6.3 Filer using more than one field
+
+
+
+# 7.2 Filer using more than one field
 # Cylinders4_mpgn <- mtcars %>% 
 # filter(cyl == "4" & mpg >"30")
 
-# 6.4 Grouping data
+
+
+# 7.3 Grouping data
 # Num_cars = mtcars %>% 
 # group_by(cyl) %>% 
 # summarise(number_of_cars = n())
 
+
+# 8 Drawing a graph
+# ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars)) +
+# geom_bar(stat="identity")
+
+# 8 Changing value to a factor
+# Num_cars <- Num_cars %>% 
+# mutate(cyl= as.factor(cyl))
+
+# 8 re-plot graph
+# ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars)) +
+# geom_bar(stat="identity")
+
+# 8.1 Change the color of the bars
+# ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars)) +
+# geom_bar(stat="identity", fill="steelblue")
+
+
+# 8.1 make the bars different colours
+# ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars, fill = cyl)) +
+# geom_bar(stat="identity")
+
+
+
+#.8.2 Adding a title to the graph
+# ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars, fill = cyl)) +
+# geom_bar(stat="identity")+
+# ggtitle("Number of cars by number of Cylinders")
+
+
+
+
+#8.3 Adding axis labels
+# ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars, fill = cyl)) +
+# geom_bar(stat="identity")+
+# ggtitle("Number of cars by number of Cylinders") +
+# xlab("Number of Cylinders") +
+# ylab("Number of Cars")
 
 
