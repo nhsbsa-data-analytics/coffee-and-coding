d4af06bcb4e88023b336b1ab2ed2170ddf485ea5
nidod <nikki.dodds@nhs.net>
2022-08-16 08:25:09 +0100

    Update Using R for the first time.R

--- 2022-08-17 Using R for the first time/Using R for the first time.R
+++ 2022-08-17 Using R for the first time/Using R for the first time.R
@@ -7,16 +7,21 @@
 # library(tidyverse)
 
 
+
 # 4.4 Getting help with a Package
 # help(package="dplyr")
+
 
 
 # 5.1 Get Working directory
 # getwd() 
 
 
+
 # 5.2 Set Working directory
 # setwd()
+
+
 
 # 6.1 Load CSV, from current directory
 # first_table <- read_csv("Mydata.csv")
@@ -27,16 +32,20 @@
 # second_table <- read_csv("C:/Users/cypher/CSVS/Mydata2.csv")
 
 
+
 # 6.2 Load excel from current directory 
 # third_table <- read_excel("Mydata3.xlsx")
+
 
 
 # 6.2 Load excel from different directory
 # fourth_table <- read_excel("C:/Users/cypher/EXCEL/Mydata4.xlsx")
 
 
+
 # 7 Simple analysis in R - Get mtcars data
 # mtcars <- mtcars
+
 
 
 # 7.1 Sum up a column
@@ -58,32 +67,48 @@
 
 
 
+
 # 7.3 Grouping data
 # Num_cars = mtcars %>% 
 # group_by(cyl) %>% 
 # summarise(number_of_cars = n())
 
 
+
+
 # 8 Drawing a graph
 # ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars)) +
 # geom_bar(stat="identity")
+
+
+
+
 
 # 8 Changing value to a factor
 # Num_cars <- Num_cars %>% 
 # mutate(cyl= as.factor(cyl))
 
+
+
+
 # 8 re-plot graph
 # ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars)) +
 # geom_bar(stat="identity")
+
+
+
 
 # 8.1 Change the color of the bars
 # ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars)) +
 # geom_bar(stat="identity", fill="steelblue")
 
 
+
+
 # 8.1 make the bars different colours
 # ggplot(data=Num_cars, aes(x= cyl, y= number_of_cars, fill = cyl)) +
 # geom_bar(stat="identity")
+
 
 
 
